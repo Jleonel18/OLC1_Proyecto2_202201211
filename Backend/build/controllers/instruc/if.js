c6c01d9bc509306786d4b1fe3ca403ada81af9ae
Jleonel18 <lopezajvixjoseleonel@gmail.com>
2024-04-20 22:00:20 -0600

    AST de If, else, else if creado,pendiente arreglos y ternario

--- Backend/build/controllers/instruc/if.js
+++ Backend/build/controllers/instruc/if.js
@@ -33,6 +33,7 @@ const tipo_1 = __importStar(require("../simbol/tipo"));
 const Break_1 = __importDefault(require("./Break"));
 const Continue_1 = __importDefault(require("./Continue"));
 const return_1 = __importDefault(require("./return"));
+const contadorSingleton_1 = __importDefault(require("../simbol/contadorSingleton"));
 class If extends instruccion_1.Instruccion {
     constructor(cond, inst, instElse, condicionElseIf, linea, columna) {
         super(new tipo_1.default(tipo_1.tipoDato.VOID), linea, columna);
@@ -110,7 +111,96 @@ class If extends instruccion_1.Instruccion {
         }
     }
     obtenerAST(anterior) {
-        return "";
+        let contador = contadorSingleton_1.default.getInstance();
+        let result = "";
+        let contInstruc = [];
+        let contInstrucElse = [];
+        let contInstrucElseIf = "";
+        let llav1Else = "";
+        let padreInstElse = "";
+        let rElse = "";
+        let llav2Else = "";
+        let rElseIf = "";
+        let rIf = `n${contador.getContador()}`;
+        let par1 = `n${contador.getContador()}`;
+        let cond = `n${contador.getContador()}`;
+        let par2 = `n${contador.getContador()}`;
+        let llav1 = `n${contador.getContador()}`;
+        let padreInstIf = `n${contador.getContador()}`;
+        for (let i = 0; i < this.instrucciones.length; i++) {
+            contInstruc.push(`n${contador.getContador()}`);
+        }
+        let llav2 = `n${contador.getContador()}`;
+        if (this.instruccionesElse != undefined) {
+            rElse = `n${contador.getContador()}`;
+            llav1Else = `n${contador.getContador()}`;
+            padreInstElse = `n${contador.getContador()}`;
+            for (let i = 0; i < this.instruccionesElse.length; i++) {
+                contInstrucElse.push(`n${contador.getContador()}`);
+            }
+            llav2Else = `n${contador.getContador()}`;
+        }
+        if (this.condicionElseIf != undefined) {
+            contInstrucElseIf = `n${contador.getContador()}`;
+            rElseIf = `n${contador.getContador()}`;
+        }
+        result += `${rIf}[label="If"];\n`;
+        result += `${par1}[label="("];\n`;
+        result += `${cond}[label="Expresion"];\n`;
+        result += `${par2}[label=")"];\n`;
+        result += `${llav1}[label="{"];\n`;
+        result += `${padreInstIf}[label="Instrucciones"];\n`;
+        for (let i = 0; i < contInstruc.length; i++) {
+            result += `${contInstruc[i]}[label="Instruccion"];\n`;
+        }
+        result += `${llav2}[label="}"];\n`;
+        if (this.instruccionesElse != undefined) {
+            result += `${rElse}[label="Else"];\n`;
+            result += `${llav1Else}[label="{"];\n`;
+            result += `${padreInstElse}[label="Instrucciones"];\n`;
+            for (let i = 0; i < contInstrucElse.length; i++) {
+                result += `${contInstrucElse[i]}[label="Instruccion"];\n`;
+            }
+            result += `${llav2Else}[label="}"];\n`;
+        }
+        if (this.condicionElseIf != undefined) {
+            result += `${contInstrucElseIf}[label="else If"];\n`;
+        }
+        result += `${anterior} -> ${rIf};\n`;
+        result += `${anterior} -> ${par1};\n`;
+        result += `${anterior} -> ${cond};\n`;
+        result += `${anterior} -> ${par2};\n`;
+        result += `${anterior} -> ${llav1};\n`;
+        result += `${anterior} -> ${padreInstIf};\n`;
+        for (let i = 0; i < contInstruc.length; i++) {
+            result += `${padreInstIf} -> ${contInstruc[i]};\n`;
+        }
+        if (this.instruccionesElse != undefined) {
+            result += `${anterior} -> ${rElse};\n`;
+            result += `${anterior} -> ${llav1Else};\n`;
+            result += `${anterior} -> ${padreInstElse};\n`;
+            for (let i = 0; i < contInstrucElse.length; i++) {
+                result += `${padreInstElse} -> ${contInstrucElse[i]};\n`;
+            }
+            result += `${anterior} -> ${llav2Else};\n`;
+        }
+        if (this.condicionElseIf != undefined) {
+            result += `${anterior} -> ${contInstrucElseIf};\n`;
+        }
+        result += `${anterior} -> ${llav2};\n`;
+        result += this.condicion.obtenerAST(cond);
+        for (let i = 0; i < contInstruc.length; i++) {
+            result += this.instrucciones[i].obtenerAST(contInstruc[i]);
+        }
+        if (this.instruccionesElse != undefined) {
+            for (let i = 0; i < contInstrucElse.length; i++) {
+                result += this.instruccionesElse[i].obtenerAST(contInstrucElse[i]);
+            }
+        }
+        if (this.condicionElseIf != undefined) {
+            result += this.condicionElseIf.obtenerAST(contInstrucElseIf);
+        }
+        return result;
     }
 }
 exports.default = If;
--- Backend/src/controllers/instruc/if.ts
+++ Backend/src/controllers/instruc/if.ts
@@ -6,6 +6,7 @@ import Tipo, { tipoDato } from "../simbol/tipo";
 import Break from "./Break";
 import Continue from "./Continue";
 import Return from "./return";
+import ContadorSingleton from "../simbol/contadorSingleton";
 
 export default class If extends Instruccion {
 
@@ -80,7 +81,125 @@ export default class If extends Instruccion {
     }
 
     obtenerAST(anterior: string): string {
-        return "";
+
+        let contador = ContadorSingleton.getInstance();
+        let result = "";
+
+        let contInstruc = [];
+        let contInstrucElse = [];
+        let contInstrucElseIf = "";
+        let llav1Else = "";
+        let padreInstElse = "";
+        let rElse = "";
+        let llav2Else ="";
+        let rElseIf = "";
+
+        let rIf = `n${contador.getContador()}`;
+        let par1 = `n${contador.getContador()}`;
+        let cond = `n${contador.getContador()}`;
+        let par2 = `n${contador.getContador()}`;
+        let llav1 = `n${contador.getContador()}`;
+        let padreInstIf = `n${contador.getContador()}`;
+
+        for(let i = 0; i < this.instrucciones.length; i++){
+            contInstruc.push(`n${contador.getContador()}`);
+        }
+
+        let llav2 = `n${contador.getContador()}`;
+
+        if(this.instruccionesElse != undefined){
+            rElse = `n${contador.getContador()}`;
+            llav1Else = `n${contador.getContador()}`;
+            padreInstElse = `n${contador.getContador()}`;
+            for(let i = 0; i < this.instruccionesElse.length; i++){
+                contInstrucElse.push(`n${contador.getContador()}`);
+            }
+            llav2Else = `n${contador.getContador()}`;
+        }
+
+        if(this.condicionElseIf != undefined){
+            contInstrucElseIf = `n${contador.getContador()}`;
+            rElseIf = `n${contador.getContador()}`;
+        }
+
+        result += `${rIf}[label="If"];\n`;
+        result += `${par1}[label="("];\n`;
+        result += `${cond}[label="Expresion"];\n`;
+        result += `${par2}[label=")"];\n`;
+        result += `${llav1}[label="{"];\n`;
+        result += `${padreInstIf}[label="Instrucciones"];\n`;
+
+        for(let i = 0; i < contInstruc.length; i++){
+            result += `${contInstruc[i]}[label="Instruccion"];\n`;
+        }
+
+        result += `${llav2}[label="}"];\n`;
+
+        if(this.instruccionesElse != undefined){
+
+            result += `${rElse}[label="Else"];\n`;
+            result += `${llav1Else}[label="{"];\n`;
+            result += `${padreInstElse}[label="Instrucciones"];\n`;
+
+            for(let i = 0; i < contInstrucElse.length; i++){
+                result += `${contInstrucElse[i]}[label="Instruccion"];\n`;
+            }
+
+            result += `${llav2Else}[label="}"];\n`;
+
+        }
+
+        if(this.condicionElseIf != undefined){
+            result += `${contInstrucElseIf}[label="else If"];\n`;
+        }
+
+        result += `${anterior} -> ${rIf};\n`;
+        result += `${anterior} -> ${par1};\n`;
+        result += `${anterior} -> ${cond};\n`;
+        result += `${anterior} -> ${par2};\n`;
+        result += `${anterior} -> ${llav1};\n`;
+        result += `${anterior} -> ${padreInstIf};\n`;
+
+        for(let i = 0; i < contInstruc.length; i++){
+            result += `${padreInstIf} -> ${contInstruc[i]};\n`;
+        }
+
+        if(this.instruccionesElse != undefined){
+            result += `${anterior} -> ${rElse};\n`;
+            result += `${anterior} -> ${llav1Else};\n`;
+            result += `${anterior} -> ${padreInstElse};\n`;
+
+            for(let i = 0; i < contInstrucElse.length; i++){
+                result += `${padreInstElse} -> ${contInstrucElse[i]};\n`;
+            }
+
+            result += `${anterior} -> ${llav2Else};\n`;
+        }
+
+        if(this.condicionElseIf != undefined){
+            result += `${anterior} -> ${contInstrucElseIf};\n`;
+        }
+
+        result += `${anterior} -> ${llav2};\n`;
+
+        result += this.condicion.obtenerAST(cond);
+
+        for(let i = 0; i < contInstruc.length; i++){
+            result += this.instrucciones[i].obtenerAST(contInstruc[i]);
+        }
+
+        if(this.instruccionesElse != undefined){
+            for(let i = 0; i < contInstrucElse.length; i++){
+                result += this.instruccionesElse[i].obtenerAST(contInstrucElse[i]);
+            }
+        }
+
+        if(this.condicionElseIf != undefined){
+            result += this.condicionElseIf.obtenerAST(contInstrucElseIf);
+        }
+
+        return result;
+
     }
 
 }
